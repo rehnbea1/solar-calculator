70cad2ac6be0f8878d1d0ccf4fd00cda39426fe2
rehnbea1 <albert.rehnberg@aalto.fi>
2022-05-27 15:01:41 +0200

    Fix calculator, add functions to implement DF calculations

--- src /app/utils/calculator.py
+++ src /app/utils/calculator.py
@@ -1,110 +1,156 @@
-from datetime import datetime
-from math import sin,cos,tan, acos, asin, radians,degrees, floor
+from datetime import datetime, timedelta
+from math import radians,degrees, floor, pi
+from sympy import *
+from math import sin
 
+class Solar:
 
-class Solar_info:
-
-    def __init__(self, irradiation, country, hemisphere, longitude, time):
+    def __init__(self, irradiation, country, hemisphere, latitude, time):
         self.country = country
         self.hemisphere = hemisphere
-        self.longitude = radians(longitude)
+        self.latitude = latitude
         self.time = self.time(time)
         self.irradiation = self.country_irradiance(self.country)
-
         self.declination_angle = self.declination_angle()
         self.solar_hour_angle = self.solar_hour_angle()
         self.solar_altitude_angle = self.solar_altitude_angle()
         self.day_length = self.day_length()
         self.azimuth = self.azimuth()
-        self.sunriseset = self.sunrise_sunset()
+        self.sun_interval = self.sun_interval()
 
 
     def time(self, time):
         if time == 0:
+            #print('no time given')
             return datetime.now()
         else:
             return datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
+
 
     def country_irradiance(self, country):
         return 500
 
 
     def solar_hour_angle(self):
+        #solar hour angle = hs
         time = self.time.hour
+        #time = self.time.time()
+        #time = time.replace(microsecond=0,second=0)
         noon = 12
         delta = time - noon
 
-        solar_hour_angle = round(radians(15 * delta),2)
+        solar_hour_angle = round((15 * delta),2)
+
         return solar_hour_angle
 
 
     def declination_angle(self):
-        tilt = radians(23.45)
+        #declination angle = ∂
+
+        tilt = 23.45
         N = self.time.timetuple().tm_yday
         sin_obj = (360/365)*(284+N)
-
-        declination_angle = round(tilt*sin(sin_obj),2)
-
+        d = tilt*sin(radians(sin_obj))
+        declination_angle = round(d,2)
         return declination_angle
 
 
     def solar_altitude_angle(self):
-        long = self.longitude
-        h = self.solar_hour_angle
-        d = self.declination_angle
-        sin_angle = sin(long)*sin(d) + cos(long)*cos(d)*cos(h)
-        altitude_angle = round(asin(sin_angle),2)
+        #alfa
+        lat = radians(self.latitude)
+        h = radians(self.solar_hour_angle)
+        d = radians(self.declination_angle)
+        sin_angle = sin(lat)*sin(d) + cos(lat)*cos(d)*cos(h)
+        angle = degrees(asin(sin_angle))
+        altitude_angle = round(angle,2)
+
         return altitude_angle
 
 
     def day_length(self):
-        lon = self.longitude
-        d = self.declination_angle
+        lat = radians(self.latitude)
+        d = radians(self.declination_angle)
         a = round(2/15,3)
-        acos_param= degrees(acos(-tan(lon)*tan(d)))
+        acos_param = acos(-tan(lat)*tan(d))
+        acos_param = degrees(acos_param)
         day_length = a*acos_param
         hours = floor(day_length)
         minutes = round((day_length-hours)*60)
-        return '%s hours and %s minutes' % (hours, minutes)
+
+        return '%s:%s' % (hours, minutes)
 
 
     def azimuth(self):
-        #Azimuth angle is alfa_s
-        h = self.solar_hour_angle
-        d = self.declination_angle
-        nom = cos(d)*sin(h)
+        try:
+            #Azimuth angle = alfa_s
+            #Azimuth is 0 at noon, negative before noon and positive after noon
 
-        a = self.solar_altitude_angle
-        denom = cos(a)
+            d = radians(self.declination_angle)
+            h = radians(self.solar_hour_angle)
+            a = radians(self.solar_altitude_angle)
 
-        azimuth = round(asin(nom/denom),2)
-        return azimuth
+            nom = cos(d)*sin(h)
+            denom = cos(a)
+            sin_alfa = nom/denom
+            azimuth = asin(sin_alfa)
+            azimuth = degrees(azimuth)
+            return azimuth
+        except TypeError:
+            print('Azimuth could not be calculated')
+            return 'Nan'
+
+
+        #d = -19,38
+        #hs = -45
+        #as = -44,3
+        #a = 17,32
+
+    def sun_interval(self):
+        #solar altitude angle should be 0
+        # sunset when cos(h) = -tanL*tan(d)
+        # calc right-side
+        # alfa
+        lat = radians(self.latitude)
+        h = symbols('h')
+        d = radians(self.declination_angle)
+        result = -(tan(lat) * tan(d))
+
+        eq1 = Eq(cos(h),-(tan(lat) * tan(d)))
+        solution = solve(eq1, h)
+        solution[1] = solution[1]-2*pi
+
+        sunrise =  12 - degrees(solution[0])/15
+        hours = floor(sunrise)
+        minutes = round((sunrise - hours) * 60)
+        sunrise = '%s:%s' % (hours, minutes)
+        sunset =  12 - degrees(solution[1])/15
+        hours = floor(sunset)
+        minutes = round((sunset - hours) * 60)
+        sunset = '%s:%s' % (hours, minutes)
+        return [sunrise,sunset]
+
 
     def __repr__(self):
         return "Class calculator object"
 
     def print(self):
-        return print('Info:', 'Country:', self.country,'| Hemisphere: ',self.hemisphere,'| Longitude: ',
-                     self.longitude,'| Irradiation: ', self.irradiation,'| Time: ',
-                     self.time,'| Declination angle (d): ',self.declination_angle,'| Solar hour angle (h):  ',
-                     self.solar_hour_angle,'| Solar altitude angle (alfa): ', self.solar_altitude_angle,
-                     '| Azimuth (alfa_s): ', self.azimuth,'| Day length: ', self.day_length)
+        return print('Info:', 'Country:', self.country,'\n| Hemisphere: ',self.hemisphere,'\n| Latitude: ',
+                     self.latitude,'\n| Irradiation: ', self.irradiation,'\n| Time: ',
+                     self.time,'\n| Declination angle (d): ',self.declination_angle,'\n| Solar hour angle (h):  ',
+                     self.solar_hour_angle,'\n| Solar altitude angle (alfa): ', self.solar_altitude_angle,
+                     '\n| Azimuth (alfa_s): ', self.azimuth,'\n| Day length: ', self.day_length, '\n| Sunrise & Sunset:', self.sun_interval)
 
-    def sunrise_sunset(self):
-        lon = self.longitude
-        d = self.declination_angle
-        acos_obj = -tan(lon)*tan(d)
-        hs = degrees(acos(acos_obj))
-        return
+    def result(self):
+        return {'country':self.country,
+                'hemisphere':self.hemisphere,
+                'latitude':self.latitude,
+                'irradiation':self.irradiation,
+                'time':self.time,
+                'declination_angle':self.declination_angle,
+                'solar_hour_angle':self.solar_hour_angle,
+                'solar_altitude_angle':self.solar_altitude_angle,
+                'azimuth_angle':self.azimuth,
+                'day_length':self.day_length,
+                'sunrise':self.sun_interval[0],
+                'sunset':self.sun_interval[1]}
 
-irradiation    = 500
-country        = 'Finland'
-hemisphere     = 'N'
-longitude      = 40
-time           = 0 #Fill in 0 if no time is given
-#'2022-06-15 14:00:00'
-object = Solar_info(irradiation,country,hemisphere,longitude, time)
-object.print()
-
-
-print('Application works!')
---
+++ src /app/utils/functions.py
@@ -0,0 +1,52 @@
+from pandas import Timestamp, to_timedelta, DataFrame
+from datetime import datetime
+from calculator import Solar
+
+
+def get_year():
+
+    time = datetime.strptime('2022-12-31 23:00:00', '%Y-%m-%d %H:%M:%S')
+    N = time.timetuple().tm_yday
+    N = N*24
+    year_df = DataFrame({'hour':[n for n in range(N+1)]})
+
+    date_root = datetime.strptime(str(datetime.now()),'%Y-%m-%d %H:%M:%S.%f')
+    date_root = datetime.strftime(date_root,format='%Y-01-01 00:00:00')
+
+    year_df['time'] = [Timestamp(date_root) + to_timedelta(year_df['hour'][row], unit='H') for row in year_df['hour']]
+    year_df = year_df.set_index('time')
+
+    return year_df
+
+def calculate(row):
+
+    irradiation = 0  # Egal for now, function not yet implemented, hardcoded above
+    country = 'Portugal, Lisbon'
+    hemisphere = 'N'
+    latitude = 40  # Lisbon
+    time = str(row)
+
+    #str(row) # Fill in 0 if no time is given, it will calculate for today
+
+    object = Solar(irradiation,country,hemisphere,latitude,time)
+    result = object.result()
+
+    return result
+
+def solar_output(year_df):
+    for row in year_df.index:
+
+        var = calculate(row)
+    print('done')
+    return
+
+def get_solar_table():
+
+    year_df = get_year()
+    output = solar_output(year_df)
+
+    return
+
+
+
+get_solar_table()
